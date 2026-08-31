7cc77cf520d545b512ffae7bee3e6a36ff30b5df
shaqalaka <shaqalaka@users.noreply.github.com>
2026-08-31 17:54:32 +1000

    Deploy hardened One More Move PWA

--- sw.js
+++ sw.js
@@ -1,12 +1,33 @@
-const CACHE = 'one-more-move-v1'
-const ASSETS = ['./', './index.html', './privacy.html', './styles.css', './game.js', './manifest.webmanifest', './icon.svg']
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))))
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))))
+const CACHE = 'one-more-move-ebcf2fd85993'
+const ASSETS = ['./', './index.html', './privacy.html', './styles.css', './game.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './icon-maskable-192.png', './icon-maskable-512.png']
+
+self.addEventListener('install', event => {
+  self.skipWaiting()
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)))
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys()
+    .then(keys => Promise.all(keys.filter(key => key.startsWith('one-more-move-') && key !== CACHE).map(key => caches.delete(key))))
+    .then(() => self.clients.claim()))
+})
+
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-    const copy = response.clone()
-    caches.open(CACHE).then(cache => cache.put(event.request, copy))
+  const request = event.request
+  if (request.method !== 'GET') return
+  const url = new URL(request.url)
+  if (url.origin !== self.location.origin) return
+
+  if (request.mode === 'navigate') {
+    event.respondWith(fetch(request).then(response => {
+      if (response.ok) caches.open(CACHE).then(cache => cache.put('./index.html', response.clone()))
+      return response
+    }).catch(() => caches.match('./index.html')))
+    return
+  }
+
+  event.respondWith(caches.match(request).then(cached => cached || fetch(request).then(response => {
+    if (response.ok) caches.open(CACHE).then(cache => cache.put(request, response.clone()))
     return response
   })))
 })
